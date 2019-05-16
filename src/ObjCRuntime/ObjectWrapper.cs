4ca9e06bc543996f5cfd4fbc63b84863d4c1b114
Aaron R Robinson <arobins@microsoft.com>
2019-05-16 16:22:45 -0700

    Remove incompatiable overlapped fields in ObjectWrapper type

--- src/ObjCRuntime/ObjectWrapper.cs
+++ src/ObjCRuntime/ObjectWrapper.cs
@@ -4,27 +4,29 @@ using System.Runtime.InteropServices;
 namespace ObjCRuntime {
 	[StructLayout(LayoutKind.Explicit)]
 	internal struct ObjectWrapper {
-		[FieldOffset(0)] object obj;
-		[FieldOffset(0)] IntPtr handle;
+		//[FieldOffset(0)] object obj;
+		//[FieldOffset(0)] IntPtr handle;
 
 		internal static IntPtr Convert (object obj) {
 			if (obj == null)
 				return IntPtr.Zero;
 
-			ObjectWrapper wrapper = new ObjectWrapper ();
+			//ObjectWrapper wrapper = new ObjectWrapper ();
 
-			wrapper.obj = obj;
+			//wrapper.obj = obj;
 
-			return wrapper.handle;
+			//return wrapper.handle;
+			return GCHandle.ToIntPtr (GCHandle.Alloc (obj));
 		}
 		
 		internal static object Convert (IntPtr ptr) 
 		{
-			ObjectWrapper wrapper = new ObjectWrapper ();
+			//ObjectWrapper wrapper = new ObjectWrapper ();
 			
-			wrapper.handle = ptr;
+			//wrapper.handle = ptr;
 				
-			return wrapper.obj;
+			//return wrapper.obj;
+			return GCHandle.FromIntPtr (ptr).Target;
 		}
 	}
 }
